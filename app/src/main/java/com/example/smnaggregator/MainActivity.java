2fb7d258a1cbc2ffd5811aee64ac1c46f5671e8e
NatTsav <natasa-98@windowslive.com>
2020-12-05 16:33:10 +0200

    Delete RegistyActivity & LoginActivity Changes in activity_main.xml Connect with facebook API and add continue with facebook button for login

--- app/src/main/java/com/example/smnaggregator/MainActivity.java
+++ app/src/main/java/com/example/smnaggregator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.smnaggregator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,28 +12,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    //buttons//
-    private Button mRegisterBtn, mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //initialize buttons//
-        mRegisterBtn = findViewById(R.id.register_btn);
-        mLoginBtn = findViewById(R.id.login_btn);
-
-        //handle register button//
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //start RegisterActivity//
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-
-            }
-        });
     }
 
 }
