c2503e49a571c1b533c2087861cb0c826a3393fa
NatTsav <natasa-98@windowslive.com>
2020-12-06 22:12:24 +0200

    Connect the user auntentication with firebase Create class MenuActivity (temporary empty)

--- app/src/main/java/com/example/smnaggregator/LoginActivity.java
+++ app/src/main/java/com/example/smnaggregator/LoginActivity.java
@@ -1,31 +1,39 @@
 package com.example.smnaggregator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.service.autofill.OnClickAction;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+    private static String EMAIL;
 
     private EditText loginEmail;
     private EditText loginPassword;
     private EditText confPassword;
     private Button loginButton;
+    private Button fbLoginBtn;
     private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -35,6 +43,11 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+            finish();
+        }
+
         loginButton=(Button)findViewById(R.id.login_btn);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +55,8 @@ public class LoginActivity extends AppCompatActivity {
                 String email=loginEmail.getText().toString().trim();
                 String password=loginPassword.getText().toString().trim();
                 String confirm=confPassword.getText().toString().trim();
+
+
 
                 if(TextUtils.isEmpty(email)){
                     loginEmail.setError("Email is Required");
@@ -66,10 +81,35 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "Passwords must be matched", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                //register the user in firebase//
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //Sign in success, sending them on another activity
+                            Log.d(TAG, "create user with email: success");
+                            Toast.makeText(LoginActivity.this, "User created", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                        }else{
+                            //If sign in fails, display a message to the user
+                            Log.w(TAG, "createUserWithEmail: failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
 
 
+
+
+
+    Log.d(TAG, "onCreate ends");
     }
+
+
 
 }
