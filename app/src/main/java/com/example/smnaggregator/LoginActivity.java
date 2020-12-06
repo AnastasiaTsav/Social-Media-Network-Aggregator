6bae0225cceaf92d722b444f8d705e54ac9dfcd7
DimitriosMoliatsidis <dai18059@uom.edu.gr>
2020-12-06 20:36:10 +0200

    layout changes button listener for local login

--- app/src/main/java/com/example/smnaggregator/LoginActivity.java
+++ app/src/main/java/com/example/smnaggregator/LoginActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,8 +20,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText loginEmail;
     private EditText loginPassword;
     private EditText confPassword;
-
     private Button loginButton;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -22,9 +29,45 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginEmail = findViewById(R.id.emailText);
-        loginPassword = findViewById(R.id.PasswwordInput);
-        confPassword = findViewById(R.id.ConfirmPassword);
+        loginEmail = (EditText)findViewById(R.id.emailText);
+        loginPassword =(EditText)findViewById(R.id.PasswwordInput);
+        confPassword =(EditText)findViewById(R.id.ConfirmPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton=(Button)findViewById(R.id.login_btn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=loginEmail.getText().toString().trim();
+                String password=loginPassword.getText().toString().trim();
+                String confirm=confPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    loginEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    loginPassword.setError("Password is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(confirm)){
+                    confPassword.setError("This field is Required");
+                    return;
+                }
+                if(password.length()<6){
+                    loginPassword.setError("The Password must be at least 6 digits");
+                    return;
+                }
+
+                if (!(password.equals(confirm))){
+                    Toast.makeText(LoginActivity.this, "Passwords must be matched", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
 
 
     }
