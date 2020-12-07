c3a0043d75f5ec777389baa3932f11255fa38ac2
DimitriosMoliatsidis <dai18059@uom.edu.gr>
2020-12-07 22:38:19 +0200

    Facebook login attempts

--- app/src/main/java/com/example/smnaggregator/LoginActivity.java
+++ app/src/main/java/com/example/smnaggregator/LoginActivity.java
@@ -7,16 +7,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +42,14 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private Button fbLoginBtn;
     private FirebaseAuth mAuth;
+    private CallbackManager mCallbackManager;
+
+    //USER INFORMATION
+    private TextView textViewUser;
+    private ImageView mLogo;
+
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private AccessTokenTracker accessTokenTracker;
 
 
     @Override
@@ -37,63 +58,64 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginEmail = (EditText)findViewById(R.id.emailText);
-        loginPassword =(EditText)findViewById(R.id.PasswwordInput);
-        confPassword =(EditText)findViewById(R.id.ConfirmPassword);
-
+        loginEmail = (EditText) findViewById(R.id.emailText);
+        loginPassword = (EditText) findViewById(R.id.PasswwordInput);
+        confPassword = (EditText) findViewById(R.id.ConfirmPassword);
+        mLogo = (ImageView) findViewById(R.id.userLogo);
+        textViewUser = (TextView) findViewById(R.id.userInfo);
         mAuth = FirebaseAuth.getInstance();
 
         //Check if the user is already logged in -> send them to an other window of the app
-        if(mAuth.getCurrentUser() != null){
+        if (mAuth.getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(), MenuActivity.class));
             finish();
         }
 
-        loginButton=(Button)findViewById(R.id.login_btn);
+
+        loginButton = (Button) findViewById(R.id.login_btn);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email=loginEmail.getText().toString().trim();
-                String password=loginPassword.getText().toString().trim();
-                String confirm=confPassword.getText().toString().trim();
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPassword.getText().toString().trim();
+                String confirm = confPassword.getText().toString().trim();
 
 
-
-                if(TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
                     loginEmail.setError("Email is Required");
                     return;
                 }
 
-                if(TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(password)) {
                     loginPassword.setError("Password is Required");
                     return;
                 }
 
-                if(TextUtils.isEmpty(confirm)){
+                if (TextUtils.isEmpty(confirm)) {
                     confPassword.setError("This field is Required");
                     return;
                 }
-                if(password.length()<6){
+                if (password.length() < 6) {
                     loginPassword.setError("The Password must be at least 6 digits");
                     return;
                 }
 
-                if (!(password.equals(confirm))){
+                if (!(password.equals(confirm))) {
                     Toast.makeText(LoginActivity.this, "Passwords must be matched", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 //register the user in firebase//
-                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             //Sign in success, sending them on another activity
                             Log.d(TAG, "create user with email: success");
                             Toast.makeText(LoginActivity.this, "User created", Toast.LENGTH_SHORT).show();
                             FirebaseUser user = mAuth.getCurrentUser();
                             startActivity(new Intent(getApplicationContext(), MenuActivity.class));
-                        }else{
+                        } else {
                             //If sign in fails, display a message to the user
                             Log.w(TAG, "createUserWithEmail: failure", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed ", Toast.LENGTH_SHORT).show();
@@ -104,13 +126,118 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        fbLoginBtn = (LoginButton) findViewById(R.id.fb_login_button);
+        mCallbackManager = CallbackManager.Factory.create();
+        ((LoginButton) fbLoginBtn).registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess" + loginResult);
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError" + error);
+            }
+
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    updateUI(user);
+                }
+                else {
+                    updateUI(null);
+                }
+            }
+        };
+
+        accessTokenTracker= new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if(currentAccessToken == null){
+                    mAuth.signOut();
+                }
+                else{
+
+                }
+            }
+        };
 
 
 
-
-    Log.d(TAG, "onCreate ends");
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        mCallbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    private void updateUI (FirebaseUser user){
+        if (user != null) {
+            textViewUser.setText(user.getDisplayName());
+            if (user.getPhotoUrl() != null) {
+                String photoUrl = user.getPhotoUrl().toString();
+                photoUrl = photoUrl + "?type=large";
+                Picasso.get().load(photoUrl).into(mLogo);
+
+            }
+        } else {
+            textViewUser.setText("");
+            mLogo.setImageResource(R.drawable.tif_logo);
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener!= null){
+            mAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+    private void handleFacebookToken (AccessToken token){
+        Log.d(TAG, "handleFacebookToken" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "SignIn with Credential Successful");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
 
 
+                } else {
+                    Log.d(TAG, "SignIn with Credential Failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+
+        });
+
+    }
 }
+
+
+
