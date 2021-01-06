41b72ea79252e330da9e833b629eb3a21b478413
DimitriosMoliatsidis <dai18059@uom.edu.gr>
2021-01-06 20:32:21 +0200

    Tweeter Trends download and print. new layout for activity_trends.xml new classes Post and JsonParser

---
+++ app/src/main/java/com/example/smnaggregator/ConstantsUtils.java
@@ -0,0 +1,16 @@
+package com.example.smnaggregator;
+
+public class ConstantsUtils {
+
+    public static final String URL_ROOT_TWITTER_API = "https://api.twitter.com";
+    public static final String URL_SEARCH = URL_ROOT_TWITTER_API + "/1.1/search/tweets.json?q=";
+    public static final String URL_AUTHENTICATION = URL_ROOT_TWITTER_API + "/oauth2/token";
+
+    public static final String URL_GREECE_TRENDING ="https://api.twitter.com/1.1/trends/place.json?id=23424833";
+
+
+    public static final String CONSUMER_KEY = "zWKtvtcgKHZAiDUEtNaPDp0jf";
+    public static final String CONSUMER_SECRET = "7x0dzyIVRcUUQSqDZx8wI84ZYrSM0ptXJL0h5wvos9GhD5aNoI";
+
+
+}
