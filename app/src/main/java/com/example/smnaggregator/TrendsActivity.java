8497accbc4f92c6d8849d419058910c31f861ec9
NatTsav <natasa-98@windowslive.com>
2021-01-12 17:15:40 +0200

    1)Display the tweets for a specific hasthtag selected from the listVied completed through twitter App 2) optimize code

--- app/src/main/java/com/example/smnaggregator/TrendsActivity.java
+++ app/src/main/java/com/example/smnaggregator/TrendsActivity.java
@@ -1,31 +1,24 @@
 package com.example.smnaggregator;
 
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 import static android.content.Intent.ACTION_VIEW;
-import static java.sql.DriverManager.println;
 
 public class TrendsActivity extends AppCompatActivity {
 
     private static final String TAG = "RestAPI";
     private static final String REMOTE_API ="https://api.twitter.com/1.1/trends/place.json?id=23424833";
 
-    private Button findTrendsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +30,7 @@ public class TrendsActivity extends AppCompatActivity {
 
                 PostArrayAdapter postArrayAdapter = new PostArrayAdapter(this,
                         R.layout.list_record,
-                        new ArrayList<Post>(),
+                        new ArrayList<>(),
                         postListView);
 
                 GetDataTask getDataTaskObject = new GetDataTask(postArrayAdapter);
@@ -45,32 +38,25 @@ public class TrendsActivity extends AppCompatActivity {
 
                 Log.d(TAG, "FINISHED downloading of trending hashtags");
 
-                postListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        //ListView postview = (ListView) postListView.getSelectedView();
-                        TextView hello = (TextView) postListView.findViewById(R.id.hahshtagURL);
-                        //ListView p = (ListView) parent.getItemAtPosition(position);
-                        //TextView hello = p.findViewById(R.id.hahshtagURL);
-                        String hi = (String) hello.getText();
-                        System.out.println(hi);
+                postListView.setOnItemClickListener((parent, view, position, id) -> {
+                    //get from the list item the field with the url and
+                    //and get the text from this to search the tweets for a specific
+                    //hashtag within the twitterApp
+                    TextView clickedView = view.findViewById(R.id.hahshtagURL);
+                    String clickedUrl = (String) clickedView.getText();
 
 
-                       // Post hello = (Post) chosen.getItemAtPosition(position);
-                        //String q = hello.getUrl();
-
-
-                       //String tweetUrl ="https://api.twitter.com/1.1/search/tweets.json?"+ q;
-                        try {
-                            startActivity(new Intent(ACTION_VIEW, Uri.parse(hi)));
-                        } catch (ActivityNotFoundException e) {
-                            e.printStackTrace();
-                        }
-
-
-
-
+                    try {
+                        startActivity(new Intent(ACTION_VIEW, Uri.parse(clickedUrl)));
+                        Log.d(TAG,"Connection with the twitter app succeed");
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                        Log.e(TAG,"Connection with twitter App failed");
                     }
+
+
+
+
                 });
             }
         }
