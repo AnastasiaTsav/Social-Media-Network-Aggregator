cd570f74f955c8a022558b4abf247254b3c147ed
NatTsav <natasa-98@windowslive.com>
2021-01-14 22:47:08 +0200

    open an image from phone gallery with permission request from the user

--- app/src/main/java/com/example/smnaggregator/MakePostActivity.java
+++ app/src/main/java/com/example/smnaggregator/MakePostActivity.java
@@ -1,14 +1,109 @@
 package com.example.smnaggregator;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MakePostActivity extends AppCompatActivity {
+
+    private EditText contentTxt;
+    private ImageButton addImageBtn;
+    private ImageView imageView;
+
+    private Switch fbSwitchBtn;
+    private Switch instSwitchBtn;
+    private Switch twitterSwitchBtn;
+
+    private Button shareBtn;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_post);
+
+        contentTxt = findViewById(R.id.makePostTxt);
+        addImageBtn = findViewById(R.id.addImageButton);
+        imageView = findViewById(R.id.imageView);
+        fbSwitchBtn = findViewById(R.id.fbPostSwitch);
+        instSwitchBtn = findViewById(R.id.instagramPostSwitch);
+        twitterSwitchBtn = findViewById(R.id.twitterPostSwitch);
+        shareBtn = findViewById(R.id.publishPostButton);
+
+        //handle addImageBtn button click//
+        addImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check runtime permission
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED){
+                        //permission not granted, request it.
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        //show popup for runtime permission
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    }
+                    else{
+                        //permission already granted
+                        pickImageFromGallery();
+                    }
+                }
+                else{
+                    //system os is less than marshmallow
+                    pickImageFromGallery();
+
+                }
+            }
+        });
+
+
+    }
+
+    private void pickImageFromGallery() {
+        //intent to pick image
+        Intent intent  = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    //handle result of runtime permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                //permission was granted
+                pickImageFromGallery();
+            } else {
+                //permission was denied
+                Toast.makeText(this, "Permission denied..!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    //handle result of picked image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            //set image to imageview
+            imageView.setImageURI(data.getData());
+        }
     }
 }
